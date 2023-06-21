3ae00c5e88c542484bf24656a68c4844252f04f7
cocomozila <joseyun94@naver.com>
2023-06-21 16:52:37 +0900

    feat(MemberRepository): 회원가입, 아이디찾기 기능 정의

---
+++ src/main/java/spin/sns/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package spin.sns.repository;
+
+import spin.sns.domain.member.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findAccount(String email);
+}
