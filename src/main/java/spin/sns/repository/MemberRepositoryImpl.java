8b2045e15fdcadf0e895fb41fae59a65191fc4fc
cocomozila <joseyun94@naver.com>
2023-06-21 16:53:53 +0900

    feat(MemberRepositoryImpl): 회원저장, 이메일로 아이디 검색 기능 추가

---
+++ src/main/java/spin/sns/repository/MemberRepositoryImpl.java
@@ -0,0 +1,40 @@
+package spin.sns.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import spin.sns.domain.member.Member;
+import spin.sns.domain.member.QMember;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class MemberRepositoryImpl implements MemberRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findAccount(String email) {
+        QMember member = QMember.member;
+        return Optional.ofNullable(
+                query
+                .select(member)
+                .from(member)
+                .where(member.email.equalsIgnoreCase(email))
+                .fetchOne());
+    }
+}
