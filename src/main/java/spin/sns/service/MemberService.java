63186c08c49e1d01d0824fde9b7fbe70ce29672e
cocomozila <joseyun94@naver.com>
2023-06-23 01:49:50 +0900

    feat(MemberService): 로그인 기능 추가 및 회원가입 중복 방지 기능 추가

--- src/main/java/spin/sns/service/MemberService.java
+++ src/main/java/spin/sns/service/MemberService.java
@@ -2,23 +2,50 @@ package spin.sns.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spin.sns.domain.member.LoginParam;
 import spin.sns.domain.member.Member;
+import spin.sns.error.exception.DuplicateEmailException;
+import spin.sns.error.exception.DuplicateNicknameException;
+import spin.sns.error.exception.MemberNotExistException;
+import spin.sns.error.exception.PasswordMismatchException;
 import spin.sns.repository.MemberRepository;
+import spin.sns.repository.SessionRepository;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final SessionRepository sessionRepository;
 
     public void signup(Member member) {
+        if (memberRepository.findByNickname(member.getNickname()).isPresent()) {
+            throw new DuplicateNicknameException("사용중인 닉네임입니다.");
+        }
+        if (memberRepository.findByEmail(member.getEmail()).isPresent()) {
+            throw new DuplicateEmailException("사용중인 이메일입니다.");
+        }
         memberRepository.save(member);
     }
 
     public String findAccount(String email) {
         return memberRepository
-                .findAccount(email)
-                .orElseThrow(RuntimeException::new)
+                .findByEmail(email)
+                .orElseThrow(() -> new MemberNotExistException("사용자를 찾을 수 없습니다."))
                 .getNickname();
+    }
+
+    public Member getLoginMember(LoginParam loginParam, HttpServletResponse response) {
+        Member member = memberRepository
+                .findByNickname(loginParam.getNickname())
+                .orElseThrow(() -> new MemberNotExistException("사용자를 찾을 수 없습니다."));
+
+        if (member.getPassword().equals(loginParam.getPassword())) {
+            sessionRepository.createSession(member, response);
+            return member;
+        }
+        throw new PasswordMismatchException("패스워드가 일치하지 않습니다.");
     }
 }
