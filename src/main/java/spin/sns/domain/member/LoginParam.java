09684747612923d3070b6cb3eb1efaa5af12ca91
cocomozila <joseyun94@naver.com>
2023-06-23 01:50:13 +0900

    feat(LoginParam): 로그인 파라미터 추가

---
+++ src/main/java/spin/sns/domain/member/LoginParam.java
@@ -0,0 +1,10 @@
+package spin.sns.domain.member;
+
+import lombok.Getter;
+
+@Getter
+public class LoginParam {
+
+    private String nickname;
+    private String password;
+}
--- src/main/java/spin/sns/domain/member/MemberSignup.java
+++ src/main/java/spin/sns/domain/member/MemberSignup.java
@@ -18,6 +18,7 @@ public class MemberSignup {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
+    @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "이메일 형식에 맞춰 입력해주세요.")
     private String email;
 
