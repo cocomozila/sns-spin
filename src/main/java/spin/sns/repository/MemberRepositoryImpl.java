0fcbfa28c8a470a9f2a06024678c4507d940068c
cocomozila <joseyun94@naver.com>
2023-07-03 13:33:32 +0900

    feat(MemberRepositoryImpl): QueryDSL 기반 MemberCustomRepository 구현

--- src/main/java/spin/sns/repository/MemberRepositoryImpl.java
+++ src/main/java/spin/sns/repository/MemberRepositoryImpl.java
@@ -4,15 +4,14 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import spin.sns.domain.member.EditPasswordParam;
-import spin.sns.domain.member.Member;
-import spin.sns.domain.member.QMember;
 
 import javax.persistence.EntityManager;
-import java.util.Optional;
+
+import static spin.sns.domain.member.QMember.member;
 
 @Repository
 @Transactional
-public class MemberRepositoryImpl implements MemberRepository {
+public class MemberRepositoryImpl implements MemberCustomRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory query;
@@ -23,42 +22,7 @@ public class MemberRepositoryImpl implements MemberRepository {
     }
 
     @Override
-    public Member save(Member member) {
-        em.persist(member);
-        return member;
-    }
-
-    @Override
-    public void deleteMember(Member member) {
-        em.remove(member);
-    }
-
-    @Override
-    public Optional<Member> findByEmail(String email) {
-        QMember member = QMember.member;
-        return Optional.ofNullable(
-                query
-                .select(member)
-                .from(member)
-                .where(member.email.equalsIgnoreCase(email))
-                .fetchOne());
-    }
-
-    @Override
-    public Optional<Member> findByNickname(String nickname) {
-        QMember member = QMember.member;
-        return Optional.ofNullable(
-                query
-                .select(member)
-                .from(member)
-                .where(member.nickname.eq(nickname))
-                .fetchOne());
-    }
-
-    @Override
     public void editPassword(EditPasswordParam editPasswordParam, String nickname) {
-        QMember member = QMember.member;
-
         query
             .update(member)
             .set(member.password, editPasswordParam.getEditPassword())
