f5ab3e1c071bd0c9de9482322a1e5b2c9b4e4a56
cocomozila <joseyun94@naver.com>
2023-07-03 18:28:28 +0900

    feat(Member, Post): Entity 수정

--- src/main/java/spin/sns/domain/member/Member.java
+++ src/main/java/spin/sns/domain/member/Member.java
@@ -3,12 +3,12 @@ package spin.sns.domain.member;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import spin.sns.domain.post.Post;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -28,6 +28,9 @@ public class Member {
     private String email;
 
     private String introduceContext;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
 
     private int followers;
     private int following;
--- src/main/java/spin/sns/domain/post/Post.java
+++ src/main/java/spin/sns/domain/post/Post.java
@@ -3,10 +3,13 @@ package spin.sns.domain.post;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import spin.sns.domain.image.Image;
 import spin.sns.domain.member.Member;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -23,8 +26,11 @@ public class Post {
     private String content;
     private int likeCount;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Image> images = new ArrayList<>();
+
     @Builder
-    public Post(Member member, LocalDateTime createTime, String content) {
+    public Post(Member member, String content) {
         this.member = member;
         this.createTime = LocalDateTime.now();
         this.content = content;
