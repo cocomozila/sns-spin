6307cb84802a567d346af83b1d2e02455677e419
cocomozila <joseyun94@naver.com>
2023-07-05 20:40:43 +0900

    feat(FollowController): 팔로우 취소 기능 추가

--- src/main/java/spin/sns/controller/FollowController.java
+++ src/main/java/spin/sns/controller/FollowController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import spin.sns.annotation.CheckLogin;
 import spin.sns.service.FollowService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,15 @@ public class FollowController {
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/{userNickname}")
+    @CheckLogin
     public void following(@PathVariable String userNickname, HttpServletRequest request) {
         followService.following(userNickname, request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{userNickname}")
+    @CheckLogin
+    public void cancelFollowing(@PathVariable String userNickname, HttpServletRequest request) {
+        followService.cancelFollowing(userNickname, request);
     }
 }
