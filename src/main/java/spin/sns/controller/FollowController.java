68e8d11cfd6815a083e964cfc5056eac9c4bd84b
cocomozila <joseyun94@naver.com>
2023-07-10 23:31:31 +0900

    feat(FollowController): 팔로워 수와 목록 조회기능 추가

--- src/main/java/spin/sns/controller/FollowController.java
+++ src/main/java/spin/sns/controller/FollowController.java
@@ -3,8 +3,10 @@ package spin.sns.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spin.sns.annotation.CheckLogin;
+import spin.sns.domain.follow.Followers;
 import spin.sns.service.FollowService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,5 +31,11 @@ public class FollowController {
     @CheckLogin
     public void cancelFollowing(@PathVariable String userNickname, HttpServletRequest request) {
         followService.cancelFollowing(userNickname, request);
+    }
+
+    @PostMapping(value = "/{userNickname}/followers")
+    public ResponseEntity<Followers> getFollowers(@PathVariable String userNickname) {
+        Followers followers = followService.getFollowers(userNickname);
+        return ResponseEntity.ok(followers);
     }
 }
