98bb53d1faf79e37d42d314e0db0290adf2fa812
cocomozila <joseyun94@naver.com>
2023-07-14 23:48:34 +0900

    feat(PostController): 게시글 삭제 기능 추가

--- src/main/java/spin/sns/controller/PostController.java
+++ src/main/java/spin/sns/controller/PostController.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import spin.sns.annotation.CheckLogin;
 import spin.sns.service.PostService;
@@ -33,5 +30,12 @@ public class PostController {
 
         postService.uploadPost(files, content, request);
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{postId}")
+    @CheckLogin
+    @ResponseStatus(HttpStatus.OK)
+    public void deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
     }
 }
