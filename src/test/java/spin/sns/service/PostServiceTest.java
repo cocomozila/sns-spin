8aaf6b1bba5c3058b50585ecd2c9e301cf89e752
cocomozila <joseyun94@naver.com>
2023-07-18 20:45:43 +0900

    test(PostServiceTest): 게시물 수정 단위테스트 추가

--- src/test/java/spin/sns/service/PostServiceTest.java
+++ src/test/java/spin/sns/service/PostServiceTest.java
@@ -9,13 +9,17 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import spin.sns.domain.member.Member;
 import spin.sns.domain.post.Post;
+import spin.sns.error.exception.PostPermissionDeniedException;
 import spin.sns.repository.MemberRepository;
 import spin.sns.repository.PostRepository;
 import spin.sns.repository.SessionRepository;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 
@@ -65,6 +69,52 @@ class PostServiceTest {
     public void deletePostTest() {
         postService.deletePost(anyLong());
         verify(postRepository, times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    @DisplayName("게시물 수정 테스트")
+    public void updatePostTest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        Member member = Member.builder()
+                .nickname("seyun")
+                .password("asdf")
+                .email("seyun94@naver.com")
+                .introduceContext("hi everyone")
+                .build();
+
+        Post post = mock(Post.class);
+
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+        when(sessionRepository.getSession(request)).thenReturn(member);
+        when(post.isSameAuthors(member)).thenReturn(true);
+
+        postService.updatePost(1L, "update", request);
+        verify(post, times(1)).updateContent(anyString());
+    }
+
+    @Test
+    @DisplayName("게시물 수정 에러 테스트 - 수정 권한 없음")
+    public void updatePostErrorTest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        Member member = Member.builder()
+                .nickname("seyun")
+                .password("asdf")
+                .email("seyun94@naver.com")
+                .introduceContext("hi everyone")
+                .build();
+
+        Post post = mock(Post.class);
+
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+        when(sessionRepository.getSession(request)).thenReturn(member);
+        when(post.isSameAuthors(member)).thenReturn(false);
+
+        PostPermissionDeniedException exception = assertThrows(PostPermissionDeniedException.class,
+                () -> postService.updatePost(1L, "update", request));
+        assertEquals("Post 수정 권한이 없습니다.", exception.getMessage());
+        verify(post, never()).updateContent(anyString());
     }
 
 }
