7f01c12b2edf800e23c7d2e4fd5922dcc396880f
cocomozila <joseyun94@naver.com>
2023-07-22 23:59:11 +0900

    feat(PostService): 좋아요 기능 추가

--- src/main/java/spin/sns/service/PostService.java
+++ src/main/java/spin/sns/service/PostService.java
@@ -7,8 +7,13 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import spin.sns.domain.member.Member;
 import spin.sns.domain.post.Post;
+import spin.sns.domain.postlike.PostLike;
+import spin.sns.error.exception.DuplicatePostLikeException;
+import spin.sns.error.exception.MemberNotExistException;
+import spin.sns.error.exception.PostNotFoundException;
 import spin.sns.error.exception.PostPermissionDeniedException;
 import spin.sns.repository.MemberRepository;
+import spin.sns.repository.PostLikeRepository;
 import spin.sns.repository.PostRepository;
 import spin.sns.repository.SessionRepository;
 
@@ -30,6 +35,9 @@ public class PostService {
 
     @Autowired
     private final SessionRepository sessionRepository;
+
+    @Autowired
+    private final PostLikeRepository postLikeRepository;
 
     @Transactional
     public void uploadPost(List<MultipartFile> files, String content,
@@ -60,5 +68,20 @@ public class PostService {
             return;
         }
         throw new PostPermissionDeniedException("Post 수정 권한이 없습니다.");
+    }
+
+    @Transactional
+    public void addPostLike(Long postId, HttpServletRequest request) {
+        Member loginMember = sessionRepository.getSession(request);
+        Member findMember = memberRepository.findById(loginMember.getMemberId())
+                .orElseThrow(()-> new MemberNotExistException("사용자를 찾을 수 없습니다."));
+
+        Post findPost = postRepository.findById(postId)
+                .orElseThrow(()-> new PostNotFoundException("게시글을 찾을 수 없습니다."));
+        if (postLikeRepository.findByMemberAndPost(findMember, findPost).isPresent()) {
+            throw new DuplicatePostLikeException("좋아요한 게시물입니다.");
+        }
+        postLikeRepository.save(new PostLike(findPost, findMember));
+        findPost.addLike();
     }
 }
