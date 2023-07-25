660e176edd8448f8595312b86e80ca694ad7ac3e
cocomozila <joseyun94@naver.com>
2023-07-25 13:37:26 +0900

    test(PostServiceTest): 좋아요 추가, 삭제 단위테스트 추가

--- src/test/java/spin/sns/service/PostServiceTest.java
+++ src/test/java/spin/sns/service/PostServiceTest.java
@@ -1,5 +1,6 @@
 package spin.sns.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import spin.sns.domain.member.Member;
 import spin.sns.domain.post.Post;
+import spin.sns.domain.postlike.PostLike;
 import spin.sns.error.exception.PostPermissionDeniedException;
 import spin.sns.repository.MemberRepository;
+import spin.sns.repository.PostLikeRepository;
 import spin.sns.repository.PostRepository;
 import spin.sns.repository.SessionRepository;
 
@@ -40,22 +43,27 @@ class PostServiceTest {
     @Mock
     private SessionRepository sessionRepository;
 
+    @Mock
+    private PostLikeRepository postLikeRepository;
+
+    private Member member;
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+
     @BeforeEach
     public void testInit() {
         MockitoAnnotations.openMocks(this);
-        postService = new PostService(postRepository, imageService, memberRepository, sessionRepository);
-    }
-
-    @Test
-    @DisplayName("게시물 업로드 테스트")
-    public void uploadPostTest() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        Member member = Member.builder()
+        postService = new PostService(postRepository, imageService, memberRepository, sessionRepository, postLikeRepository);
+        member = Member.builder()
                 .nickname("seyun")
                 .password("asdf")
                 .email("seyun94@naver.com")
                 .introduceContext("hi everyone")
                 .build();
+    }
+
+    @Test
+    @DisplayName("게시물 업로드 테스트")
+    public void uploadPostTest() {
 
         when(sessionRepository.getSession(request)).thenReturn(member);
         when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
@@ -74,14 +82,6 @@ class PostServiceTest {
     @Test
     @DisplayName("게시물 수정 테스트")
     public void updatePostTest() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-
-        Member member = Member.builder()
-                .nickname("seyun")
-                .password("asdf")
-                .email("seyun94@naver.com")
-                .introduceContext("hi everyone")
-                .build();
 
         Post post = mock(Post.class);
 
@@ -96,14 +96,6 @@ class PostServiceTest {
     @Test
     @DisplayName("게시물 수정 에러 테스트 - 수정 권한 없음")
     public void updatePostErrorTest() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-
-        Member member = Member.builder()
-                .nickname("seyun")
-                .password("asdf")
-                .email("seyun94@naver.com")
-                .introduceContext("hi everyone")
-                .build();
 
         Post post = mock(Post.class);
 
@@ -117,4 +109,46 @@ class PostServiceTest {
         verify(post, never()).updateContent(anyString());
     }
 
+    @Test
+    @DisplayName("좋아요 등록 테스트")
+    public void addPostLikeTest() {
+
+        Post post = Post.builder()
+                .member(member)
+                .content("test")
+                .build();
+
+        when(sessionRepository.getSession(request)).thenReturn(member);
+        when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+        when(postLikeRepository.findByMemberAndPost(member, post)).thenReturn(Optional.empty());
+        postService.addPostLike(anyLong(), request);
+
+        verify(postLikeRepository, times(1)).save(any(PostLike.class));
+        Assertions.assertThat(post.getLikeCount()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("좋아요 삭제 테스트")
+    public void deletePostLikeTest() {
+
+        Post post = Post.builder()
+                        .member(member)
+                        .content("test")
+                        .build();
+
+        PostLike postLike = PostLike.builder()
+                        .post(post)
+                        .member(member)
+                        .build();
+
+        when(sessionRepository.getSession(request)).thenReturn(member);
+        when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+        when(postLikeRepository.findByMemberAndPost(member, post)).thenReturn(Optional.of(postLike));
+
+        postService.deletePostLike(anyLong(), request);
+        verify(postLikeRepository,  times(1)).delete(postLike);
+        Assertions.assertThat(post.getLikeCount()).isEqualTo(-1);
+    }
 }
